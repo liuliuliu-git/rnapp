fc813886f03cbf3adacefb63c7c1f4a60419eac1
林炳成 <3078421415@qq.com>
2025-11-01 21:39:48 +0800

    增加课程的列表渲染

--- src/app/(tabs)/index.js
+++ src/app/(tabs)/index.js
@@ -1,44 +1,42 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { Link } from 'expo-router'
+import {
+  ScrollView,
+  StyleSheet,
+} from 'react-native'
 import useFetchData from '@/hooks/useFetchData'
-
-
+import Loading from '@/components/shared/Loading'
+import NetworkError from '@/components/shared/NetworkError'
+import Slides from '@/components/(tabs)/index/Slides'
+import CoursesList from '@/components/(tabs)/index/CoursesList'
 export default function Index() {
   const url = '/'
   const { data, loading, error, onReload } = useFetchData(url)
-  console.log(data)
+  const { recommendedCourses, likesCourses, introductoryCourses } = data
+
+  // 加载中
+  if (loading) {
+    return <Loading />
+  }
+
+  // 网络错误
+  if (error) {
+    return <NetworkError onReload={onReload} />
+  }
+
+
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>这里是首页</Text>
-
-      <Link style={styles.link} href="/courses/1?title=Node.js">
-        查看 Node.js 课程
-      </Link>
-
-      <Link style={styles.link} href="/teachers/1">
-        打开教师页（Modal）
-      </Link>
-    </View>
+    <ScrollView style={styles.container}>
+      <Slides courses={recommendedCourses} />
+      <CoursesList courses={likesCourses} title="热门课程" />
+      <CoursesList courses={introductoryCourses} title="入门课程" />
+    </ScrollView>
   )
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: '#e29447',
-  },
-  link: {
-    marginTop: 20,
-    fontSize: 20,
-    color: '#1f99b0',
-  },
+
 })
