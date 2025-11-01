4f7306c151553d22375ce1843d20916e89b25b76
林炳成 <3078421415@qq.com>
2025-11-01 19:58:55 +0800

    实现ios上的液态玻璃效果

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -2,6 +2,7 @@ import { Tabs, Link } from 'expo-router'
 import { Image } from 'expo-image'
 import { SimpleLineIcons } from '@expo/vector-icons'
 import { StyleSheet, TouchableOpacity } from 'react-native'
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs'
 
 /**
  * 导航栏 Logo 组件
@@ -40,53 +41,72 @@ function TabBarIcon(props) {
   return <SimpleLineIcons size={25} {...props} />
 }
 
+
+
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerTitleAlign: 'center', // 安卓标题栏居中
-        headerTitle: (props) => <LogoTitle {...props} />,
-        headerLeft: () => <HeaderButton name="bell" href="/articles" style={style.headerLeft} />,
-        headerRight: () => (
-          <>
-            <HeaderButton name="magnifier" href="/search" style={style.headerRight} />
-            <HeaderButton name="options" href="/settings" style={style.headerRight} />
-          </>
-        ),
-        tabBarActiveTintColor: '#1f99b0', // 设置 TabBar 选中项的颜色
-        // Android 取消水波纹效果
-        tabBarButton: (props) => (
-          <TouchableOpacity
-            {...props}
-            activeOpacity={1}
-            style={[props.style, { backgroundColor: 'transparent' }]}
-          />
-        ),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: '发现',
-          tabBarIcon: ({ color }) => <TabBarIcon name="compass" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="videos"
-        options={{
-          title: '视频课程',
-          tabBarIcon: ({ color }) => <TabBarIcon name="camrecorder" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="users"
-        options={{
-          title: '我的',
-          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-        }}
-      />
-    </Tabs>
+    <NativeTabs tintColor="#1f99b0" disableTransparentOnScrollEdge>
+      <NativeTabs.Trigger name="index">
+        <Label>发现</Label>
+        <Icon sf={{ default: 'play.house', selected: 'play.house.fill' }} />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="videos">
+        <Label>视频课程</Label>
+        <Icon sf={{ default: 'video', selected: 'video.fill' }} />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="users">
+        <Label>我的</Label>
+        <Icon sf={{ default: 'person', selected: 'person.fill' }} />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   )
+
+  // return (
+  //   <Tabs
+  //     screenOptions={{
+  //       headerTitleAlign: 'center', // 安卓标题栏居中
+  //       headerTitle: (props) => <LogoTitle {...props} />,
+  //       headerLeft: () => <HeaderButton name="bell" href="/articles" style={style.headerLeft} />,
+  //       headerRight: () => (
+  //         <>
+  //           <HeaderButton name="magnifier" href="/search" style={style.headerRight} />
+  //           <HeaderButton name="options" href="/settings" style={style.headerRight} />
+  //         </>
+  //       ),
+  //       tabBarActiveTintColor: '#1f99b0', // 设置 TabBar 选中项的颜色
+  //       // Android 取消水波纹效果
+  //       tabBarButton: (props) => (
+  //         <TouchableOpacity
+  //           {...props}
+  //           activeOpacity={1}
+  //           style={[props.style, { backgroundColor: 'transparent' }]}
+  //         />
+  //       ),
+  //     }}
+  //   >
+  //     <Tabs.Screen
+  //       name="index"
+  //       options={{
+  //         title: '发现',
+  //         tabBarIcon: ({ color }) => <TabBarIcon name="compass" color={color} />,
+  //       }}
+  //     />
+  //     <Tabs.Screen
+  //       name="videos"
+  //       options={{
+  //         title: '视频课程',
+  //         tabBarIcon: ({ color }) => <TabBarIcon name="camrecorder" color={color} />,
+  //       }}
+  //     />
+  //     <Tabs.Screen
+  //       name="users"
+  //       options={{
+  //         title: '我的',
+  //         tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+  //       }}
+  //     />
+  //   </Tabs>
+  // )
 }
 
 const style = StyleSheet.create({
--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,41 +1,17 @@
-import { Stack, useRouter } from 'expo-router'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import { TouchableOpacity, View } from 'react-native'
-
+import { Stack } from 'expo-router'
+import tabOptions from '@/options/tabOptions'
+import screenOptions from '@/options/screenOptions'
+import ModalCloseButton from '@/components/shared/ModalCloseButton'
 /**
- * 模态页关闭按钮组件
+ * 全局布局配置
  */
-function CloseButton() {
-  const router = useRouter()
-
-  return (
-    <View style={{ width: 30 }}>
-      <TouchableOpacity onPress={() => router.dismiss()}>
-        <MaterialCommunityIcons name="close" size={30} color="#1f99b0" />
-      </TouchableOpacity>
-    </View>
-  )
-}
-
 export default function Layout() {
   return (
     <Stack
-      screenOptions={{
-        title: '', // 默认标题为空
-        headerTitleAlign: 'center', // 安卓标题栏居中
-        animation: 'slide_from_right', // 安卓使用左右切屏
-        headerTintColor: '#1f99b0', // 导航栏中文字、按钮、图标的颜色
-        // 标题组件的样式
-        headerTitleStyle: {
-          fontWeight: '400',
-          color: '#2A2929',
-          fontSize: 16,
-        },
-        headerBackButtonDisplayMode: 'minimal', // 设置返回按钮只显示箭头，不显示文字
-      }}
+      screenOptions={screenOptions}
     >
       {/* Tabs */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={tabOptions} />
 
       {/* Cards */}
       <Stack.Screen name="articles/index" options={{ title: '通知' }} />
@@ -50,7 +26,7 @@ export default function Layout() {
           presentation: 'modal',
           animation: 'slide_from_bottom', // 从底部滑入
           title: '老师详情',
-          headerLeft: () => <CloseButton />,
+          headerLeft: () => <ModalCloseButton />,
         }}
       />
     </Stack>
